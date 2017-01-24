f4ab4b3babcc149755f63972c64df35ada289805
Chris Lee <z1821464@turing.cs.niu.edu>
2017-01-24 15:41:56 -0600

    it works now

--- assignment00.cc
+++ assignment00.cc
@@ -5,3 +5,15 @@
     I certify that this is my own work and where appropriate an extension 
     of the starter code provided for the assignment.
 */
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  cout << "Turing and Hopper is confusing to use" << endl;
+  cout << "I'm very excited to be taking Data Structures" << endl;
+
+  return 0;
+
+}// End of main() routine.
